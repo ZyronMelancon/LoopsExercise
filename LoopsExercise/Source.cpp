4ee85902a2a1b8a5f4f2aba9214f07f68642df4e
Zyron Melancon <altausolaris@gmail.com>
2016-08-19 16:10:45 -0500

    Exercise 4
    Made "while" versions of exercises 1-3.

--- LoopsExercise/Source.cpp
+++ LoopsExercise/Source.cpp
@@ -35,8 +35,8 @@ int exercise2()
 			cout << i << endl;
 		}
 	}
-	system("pause");
 
+	system("pause");
 	return 0;
 }
 
@@ -74,10 +74,79 @@ int exercise3()
 	return 0;
 }
 
+int exercise4()
+{
+	int i = 100;
+
+	while (i > 0)
+	{
+		cout << i << endl;
+		i -= 2;
+	}
+	system("pause");
+
+	i = 0;
+
+	while (i < 101)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			cout << "FizzBuzz" << endl;
+		}
+		else if (i % 3 == 0 && i % 5 != 0)
+		{
+			cout << "Fizz" << endl;
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			cout << "Buzz" << endl;
+		}
+		else
+		{
+			cout << i << endl;
+		}
+		i++;
+	}
+	system("pause");
+
+	i = 1;
+
+	int multiple[1000];
+	int sum = 0;
+
+	while (i < 1001)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			multiple[i] = i;
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			multiple[i] = i;
+		}
+		else
+		{
+			multiple[i] = 0;
+		}
+		i++;
+	}
+
+	i = 1;
+
+	while (i < 1001)
+	{
+		sum = multiple[i] + sum;
+		i++;
+	}
+
+	cout << "The sum of all multiples of 3 or 5 under 1000 is: " << sum << endl;
+	system("pause");
+
+	return 0;
+}
+
 int main()
 {
-	exercise1();
-	exercise2();
-	exercise3();
+	exercise4();
 	return 0;
 }
